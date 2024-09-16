96edf46175096985d7a69276db4a8ff52d725c18
juanbolivar <juanbolivarf20@gmail.com>
2024-09-16 09:40:54 -0500

    react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import myLogo from './assets/img/logoReact.png'
+
+function App() { 
+  return (
+    <>
+      <div>
+        <img src={myLogo} alt="React logo" />
+        <h1> Hello World </h1>
+        <p> This is my first React project... </p>
+      </div>
+    </>
+  )
+}
+
+export default App
